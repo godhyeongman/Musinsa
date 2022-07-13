64933d286c0b6bf5cfbd5dd6cd2e7a4b142c857d
godhyeongman <aaaa4000@naver.com>
2022-07-14 00:08:51 +0900

    rename: 색상 관련 네이밍 gray4 -> tabBorder 사용성에 맞춰서 명시적 네이밍 변경

--- src/components/filter/Tab.tsx
+++ src/components/filter/Tab.tsx
@@ -27,7 +27,7 @@ const TabWrapper = styled.button<Partial<TabProps>>`
   display: flex;
   gap: 7px;
   padding: 7px 15px;
-  border: 1px solid ${({ theme }) => theme.color.gray4};
+  border: 1px solid ${({ theme }) => theme.color.tabBorder};
   border-radius: 18px;
 
   ${({ backgroundColor }) => css`
