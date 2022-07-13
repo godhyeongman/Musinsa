5e054989f8abe5d4776c66952a5bc0941814f0f8
godhyeongman <aaaa4000@naver.com>
2022-07-13 22:39:40 +0900

    rename: 필터 데이터 관련 상수값 컨벤션에 맞게 네이밍 변경

--- src/templates/DefaultHeader.tsx
+++ src/templates/DefaultHeader.tsx
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
 import Logo from '@/assets/icons/Logo.svg';
-import TabBar from '@/components/TabBar';
-import Tab from '@/components/Tab';
-import filterTabContents from '@/constants/FilterTabContents';
+import TabBar from '@/components/filter/TabBar';
+import Tab from '@/components/filter/Tab';
+import FILTER_TAB_DATA from '@/constants/FilterTabContents';
 
 // TODO: a 태그 추후 리액트 라우터 돔을 이용하여 Link태그로 개선가능
 function DefaultHeader() {
-  const filterTabItem = filterTabContents.map(({ contents, ownIcon }) => (
+  const filterTabItem = FILTER_TAB_DATA.map(({ contents, ownIcon }) => (
     <Tab contents={contents} Icon={ownIcon} />
   ));
 
