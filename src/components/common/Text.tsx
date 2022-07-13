c56b3a067c2ca61c77bbadb77052687809e4b4fe
godhyeongman <aaaa4000@naver.com>
2022-07-13 22:07:43 +0900

    feat: Text 컴포넌트 범용성을 위한 styles 속성 추가

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -6,22 +6,35 @@ type TextProps = {
   fontSize?: 'large' | 'medium' | 'small';
   fontWeight?: 'large' | 'medium' | 'small';
   color?: keyof typeof THEME.color;
+  styles?: any; // 범용성을 위해
 };
 
-function Text({ contents, fontSize, fontWeight, color }: TextProps) {
+function Text({ contents, fontSize, fontWeight, color, styles }: TextProps) {
   return (
-    <StyledSpan fontSize={fontSize} fontWeight={fontWeight} color={color}>
+    <StyledSpan
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      color={color}
+      styles={styles}
+    >
       {contents}
     </StyledSpan>
   );
 }
 
 const StyledSpan = styled.span<Partial<TextProps>>`
-  ${({ theme, color = 'black', fontSize = 'medium', fontWeight = 'medium' }) =>
+  ${({
+    theme,
+    color = 'black',
+    fontSize = 'medium',
+    fontWeight = 'medium',
+    styles,
+  }) =>
     css`
       color: ${theme.color[color]};
       font-weight: ${theme.fontWeight[fontWeight]};
       fontsize: ${theme.fontSize[fontSize]};
+      ${styles};
     `}
 `;
 
