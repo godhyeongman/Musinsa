9b63923cac4d23594abbd22c74a873838e4518c2
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-13 13:02:04 +0900

    chore: 웹팩 설정 모드별 분리 세팅

---
+++ config/webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+  entry: { app: path.join(__dirname, "src", "index.tsx") },
+
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: { browsers: ["> 0.2% in KR, not dead"] },
+                debug: true,
+                modules: false,
+                useBuiltIns: "usage",
+                corejs: 3,
+              },
+            ],
+            ["@babel/preset-react"],
+            "@babel/preset-typescript",
+          ],
+          plugins: ["react-refresh/babel", "babel-plugin-styled-components"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
