32e1a73894465ff7c257beadb74c3d7af456ac2b
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-14 18:37:00 +0900

    feat: 할인률 컴포넌트 구현

---
+++ src/components/product/DiscountRateText.tsx
@@ -0,0 +1,16 @@
+import Text from '@/components/common/Text';
+
+type DiscountRateProps = { discountRate: string };
+
+function DiscountRateText({ discountRate }: DiscountRateProps) {
+  return (
+    <Text
+      contents={discountRate}
+      fontSize="large"
+      fontWeight="large"
+      color="red"
+    />
+  );
+}
+
+export default DiscountRateText;
