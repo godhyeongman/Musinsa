5807aa1c2b707f4bfd665ccfbba8fd0df60dc980
godhyeongman <aaaa4000@naver.com>
2022-07-14 21:50:13 +0900

    feat: 상품카드 프롭스 및 타입 추가

--- src/templates/ProductCard.tsx
+++ src/templates/ProductCard.tsx
@@ -1,9 +1,29 @@
 import styled from 'styled-components';
 import * as Product from '@/components/product';
 
-function ProductCard() {
-  const testPrice = 10000000;
+type ProductCardProps = {
+  id: number;
+  brandName: string;
+  imageUrl: string;
+  linkUrl: string;
+  brandLinkUrl: string;
+  isSale: boolean;
+  isSoldOut: boolean;
+  isExclusive: boolean;
+  saleRate: number;
+};
 
+function ProductCard({
+  id,
+  brandName,
+  imageUrl,
+  linkUrl,
+  brandLinkUrl,
+  isSale,
+  isSoldOut,
+  isExclusive,
+  saleRate,
+}: Partial<ProductCardProps>) {
   return (
     <ProductCardWrapper>
       <ProductFigure>
@@ -14,12 +34,12 @@ function ProductCard() {
       </ProductFigure>
       <InfoSection>
         <Product.BrandNameText name="브랜드 이름" />
-        <Product.ProductNameText name="상품명" />
+        <Product.ProductNameText name="상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명" />
         <SaledPrice>
-          <Product.CurrentPriceText price={testPrice.toLocaleString()} />
-          <Product.DiscountRateText discountRate={String(30)} />
+          <Product.CurrentPriceText price="dummy" />
+          <Product.DiscountRateText discountRate="333333" />
         </SaledPrice>
-        <Product.OriginalPriceText originalPrice={testPrice.toLocaleString()} />
+        <Product.OriginalPriceText originalPrice="dummy" />
       </InfoSection>
     </ProductCardWrapper>
   );
@@ -38,11 +58,13 @@ const ProductImg = styled.img``;
 const InfoSection = styled.section`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   padding: 20px 10px;
 `;
 
 const SaledPrice = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
 `;
 
