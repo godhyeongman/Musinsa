2a639c469c6b897ed1a541b950dc3179199f5851
godhyeongman <aaaa4000@naver.com>
2022-07-15 00:07:47 +0900

    fix: 제품 카드 key 프롭스 추가

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -15,6 +15,7 @@ export function Home() {
   );
 
   let test;
+
   if (productsData.payLoad) {
     const {
       payLoad: {
@@ -50,6 +51,7 @@ export function Home() {
           imageUrl={imageUrl}
           linkUrl={linkUrl}
           brandLinkUrl={brandLinkUrl}
+          key={goodsNo}
         />
       ),
     );
