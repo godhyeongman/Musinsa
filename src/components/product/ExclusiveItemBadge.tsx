cfc294e0b7fc889aaba73cddf37791567f0ee4bd
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-15 14:09:23 +0900

    feat: 단독 상품 뱃지 컴포넌트 구현

---
+++ src/components/product/ExclusiveItemBadge.tsx
@@ -0,0 +1,22 @@
+import styled, { css } from 'styled-components';
+
+export function ExclusiveItemBadge({ isExclusive }: { isExclusive: boolean }) {
+  console.log(isExclusive);
+  return <StyledBadge isExclusive={isExclusive}>단독</StyledBadge>;
+}
+
+const StyledBadge = styled.h4<{ isExclusive: boolean }>`
+  padding: 4px 6px;
+  position: absolute;
+  left: 10px;
+  bottom: -10px;
+  ${({ theme, isExclusive }) => css`
+    display: ${isExclusive ? 'block' : 'none'};
+    color: ${theme.color.white};
+    background-color: ${theme.color.badgeGreen};
+    font-size: ${theme.fontSize.small};
+    font-weight: ${theme.fontWeight.medium};
+  `};
+`;
+
+export default ExclusiveItemBadge;
