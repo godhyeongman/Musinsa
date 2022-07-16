018916927505e2df4e17c3b10fd39fdc9c3b4fc1
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-16 18:49:37 +0900

    feat: 헤더 탭에 데이터 필터링 이벤트 추가.

--- src/templates/DefaultHeader.tsx
+++ src/templates/DefaultHeader.tsx
@@ -8,12 +8,15 @@ import { useNullGuardedContext } from '@/hooks/useNullGuardedContext';
 import {
   ToggleStateContext,
   ToggleStateDispatchContext,
-} from '@/contexts/DisplayToggleProvider';
+} from '@/contexts/DisPlayToggle/DisplayToggleProvider';
+import { ProductsFilterDispatchContext } from '@/contexts/ProductsFilter/ProductsFilterProvider';
 
-// TODO: a 태그 추후 리액트 라우터 돔을 이용하여 Link태그로 개선가능
 function DefaultHeader() {
   const toggleDisplayState = useNullGuardedContext(ToggleStateContext);
   const { setToggle } = useNullGuardedContext(ToggleStateDispatchContext);
+  const filteringDispatch = useNullGuardedContext(
+    ProductsFilterDispatchContext,
+  );
 
   const toggleFilterInput = (
     e: React.MouseEvent<HTMLElement>,
@@ -23,21 +26,42 @@ function DefaultHeader() {
 
     setToggle(currentDisplayState);
   };
+
+  const handleFilterTab = (
+    actionType:
+      | 'CLICKED_SALE'
+      | 'CLICKED_EXCLUSIVE'
+      | 'CLICKED_SOLD_OUT'
+      | 'RESET',
+  ) => {
+    const action = { type: actionType };
+    filteringDispatch(action);
+  };
   // Todo: 리팩토링 -> 검색 버튼을 따로 만들것인가에 대해서 고민할것 그럴경우 if문 사라짐
-  const filterTabItem = FILTER_TAB_DATA.map(({ contents, ownIcon }, idx) => {
-    if (idx === 0) {
+  const filterTabItem = FILTER_TAB_DATA.map(
+    ({ contents, ownIcon, actionType }, idx) => {
+      if (idx === 0) {
+        return (
+          <Tab
+            contents={contents}
+            Icon={ownIcon}
+            onClickHandler={e => {
+              toggleFilterInput(e, toggleDisplayState);
+            }}
+          />
+        );
+      }
       return (
         <Tab
           contents={contents}
           Icon={ownIcon}
-          onClickHandler={e => {
-            toggleFilterInput(e, toggleDisplayState);
+          onClickHandler={() => {
+            handleFilterTab(actionType!);
           }}
         />
       );
-    }
-    return <Tab contents={contents} Icon={ownIcon} />;
-  });
+    },
+  );
 
   return (
     <>
